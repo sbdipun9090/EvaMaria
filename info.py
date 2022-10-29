677c49787102cbc8e126e99b7451b22be600e067
sbdipun9090 <100336442+sbdipun9090@users.noreply.github.com>
2022-10-29 14:43:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
+API_ID = int(environ['API_ID', "7405235"])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
